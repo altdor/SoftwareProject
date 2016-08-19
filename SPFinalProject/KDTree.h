b312f1e329ecfe7196fdba336e703182238465c6
softwareproject <altdor8@gmail.com>
2016-08-19 11:58:08 +0300

    KDTREE UPDATE

--- SPFinalProject/KDTree.h
+++ SPFinalProject/KDTree.h
@@ -2,6 +2,13 @@
 
 typedef struct sp_KDTree_Node* SPKDTreeNode;
 
+typedef enum sp_kd_tree_msg_t {
+	SP_KDTREE_OUT_OF_MEMORY,
+	SP_KDTREE_FULL,
+	SP_KDTREE_EMPTY,
+	SP_KDTREE_INVALID_ARGUMENT,
+	SP_KDTREE_SUCCESS
+} SP_KDTREE_MSG;
 
 ##build a tree
 KDTreeNode buildKDTree(SPKDArray array, int depth)
